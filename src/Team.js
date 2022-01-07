38090d802e7b6b1efac47c5f619e00280e7868e4
EsotericZ <cjsand03@gmail.com>
2022-01-07 15:31:29 -0700

    fixed some thing with auto wrtie code

--- src/Team.js
+++ src/Team.js
@@ -1,12 +1,14 @@
 Team = (teamAwesome) => {
     console.log(teamAwesome)
-    const man1 = members(teamAwesome);
+    const htmlInside = members(teamAwesome);
     const htmlFull = 
-        `<!DOCTYPE html>
+        `
+<!DOCTYPE html>
 <html lang="en">
 <head>
-    <meta charset="UTF-8" />
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.0/css/bootstrap.min.css"> -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.1/css/all.css" integrity="sha384-50oBUHEmvpQ+1lW4y57PTFmhCaXp0ML5d60M1M7uH2+nqUivzIebhndOJK28anvf" crossorigin="anonymous"/>
     <link href="https://cdnjs.cloudflare.com/ajax/libs/mdb-ui-kit/3.6.0/mdb.min.css" rel="stylesheet"/>
@@ -21,14 +23,15 @@ Team = (teamAwesome) => {
 
 <body>
     <div class="container">
-        <div class="row">
-            ${man1}
-        </div>
+    <div class="row justify-content-center">
+            ${htmlInside}
     </div>
+    </div>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
 </body>
 </html>
         `;
-    return htmlFull;
+    return htmlInside;
 }
 
 let roll, name, id, email, office, github, school, job;
@@ -92,7 +95,7 @@ members = (teamAwesome) => {
                     <div class="card-body">
                         <p class="card-text">ID: ${id}</p>
                         <p class="card-text">Email: <a href="mailto:${email}">${email}</a></p>
-                        <p class="card-text">School:${intern.school}</p>
+                        <p class="card-text">School:${school}</p>
                     </div>
                 </div>
             </div>
@@ -100,7 +103,7 @@ members = (teamAwesome) => {
             htmlPart += job;
         }
     });
-    return ;
+    return htmlPart;
 }
 
 module.exports = Team;
